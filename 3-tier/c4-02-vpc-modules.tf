61aed43c016c454b3e8946da05f709d1a40b18dd
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-05 16:52:19 +0530

    Update c4-02-vpc-modules.tf

--- 3-tier/c4-02-vpc-modules.tf
+++ 3-tier/c4-02-vpc-modules.tf
@@ -16,5 +16,25 @@ modules "vpc" {
   create_database_subnet_group = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
 
-  enable_vpc_nat_gateway=true
-  enable_vpc_subnet_gateway=true
+  enable_nat_gateway=true
+  enable_subnet_gateway=true
+
+tags=local.common_tags
+vpc_tags=local.vpc_tags
+
+public_subnet_tags {
+
+ Type = "Public Subnet"
+}
+
+private_subnet_tags {
+
+ Type = "Private Subnet"
+
+}
+
+database_subnet_tags {
+
+ Type= "Private database_subnet"
+
+}
