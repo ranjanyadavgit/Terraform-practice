d00f19726ad4d876f2efabc12cb88206286a4b50
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-05 16:06:00 +0530

    Create c2-generic-variables.tf

---
+++ 3-tier/c2-generic-variables.tf
@@ -0,0 +1,17 @@
+
+variable "aws_region" {
+  description = "the value of aws region"
+  value = "us-east-1"
+  }
+
+variable "environment" {
+  description="the value of the environment"
+  type=string
+   default="stage"
+  }
+
+variable "business" {
+  description="the value of the business segment"
+  type=string
+  default="SAP"
+}
